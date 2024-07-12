94e910f069f77b82b2f80535089cce9b09e5eed1
Marcos Brito <marcos.brito0507@gmail.com>
2024-07-12 06:43:54 -0300

    feat: create some default templates

--- src/template.rs
+++ src/template.rs
@@ -1,24 +1,68 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
 use serde::Serialize;
+use std::path::Path;
 use tera::Tera;
 
-pub struct Templater {}
+#[derive(Debug, Clone)]
+pub struct Templater {
+    engine: Tera,
+}
 
 impl Templater {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            engine: Tera::default(),
+        }
     }
 }
 
 impl Templater {
-    pub fn render_raw<S, T>(template: S, data: T) -> Result<String>
+    pub fn render<T>(&self, template_name: &str, data: &T) -> Result<String>
     where
         T: Serialize,
-        S: Into<String>,
     {
-        let mut tera = Tera::default();
         let context = tera::Context::from_serialize(data)?;
 
-        Ok(tera.render_str(&template.into(), &context)?)
+        Ok(self.engine.render(&template_name, &context)?)
+    }
+
+    pub fn add_template<P>(&mut self, path: P) -> Result<()>
+    where
+        P: AsRef<Path>,
+    {
+        // Is it safe to unwrap?
+        let template_name = &path
+            .as_ref()
+            .file_stem()
+            .unwrap()
+            .to_string_lossy()
+            .to_string();
+
+        self.engine.add_template_file(&path, Some(template_name))?;
+
+        Ok(())
+    }
+
+    pub fn add_raw_template(&mut self, name: &str, template: &str) -> Result<()> {
+        self.engine.add_raw_template(&name, &template)?;
+
+        Ok(())
+    }
+
+    pub fn render_raw<T>(&mut self, template: &str, data: &T) -> Result<String>
+    where
+        T: Serialize,
+    {
+        let context = tera::Context::from_serialize(data)?;
+
+        Ok(self.engine.render_str(&template, &context)?)
+    }
+}
+
+impl Default for Templater {
+    fn default() -> Self {
+        Self {
+            engine: Tera::new("templates/**").expect("Default templates should not fail to load"),
+        }
     }
 }
